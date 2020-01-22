e409217825ea1129055f7a927828b2731aadaf59
EMMANOEL <emmanoel@asav.org.br>
2020-01-22 17:18:41 -0300

    Corrigindo load da lista de contatos

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,5 @@
+import 'package:bytebanksqlite/database/app.database.dart';
+import 'package:bytebanksqlite/models/contact.dart';
 import 'package:bytebanksqlite/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
@@ -8,33 +10,74 @@ class ContactsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Alex',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          )
-        ],
-      ),
+      body: FutureBuilder<List<Contact>>(
+          initialData: List(),
+          future: findAll(),
+          builder: (context, snapshot) {
+            switch(snapshot.connectionState){
+
+              case ConnectionState.none:
+                break;
+              case ConnectionState.waiting:
+                return Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        CircularProgressIndicator(),
+                      ]),
+                );
+                break;
+              case ConnectionState.active:
+                break;
+              case ConnectionState.done:
+                final List<Contact> contacts = snapshot.data;
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Contact contact = contacts[index];
+                    return _ContactItem(contact);
+                  },
+                  itemCount: contacts.length,
+                );
+                break;
+            }
+            return Text("Erro desconhecido");
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => ContactForm(),
-            ),
-          ).then((newContact) =>debugPrint(newContact.toString()));
+          Navigator.of(context)
+              .push(
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
+              .then((newContact) => debugPrint(newContact.toString()));
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
